80035d853301b3824f094ba0d6905c2a1638e0ff
PatriciaDurand <durand_patricia_@hotmail.com>
2015-04-13 14:10:33 -0300

    Lendo os dados do arquivo

--- src/escalonamento/Escalonamento.java
+++ src/escalonamento/Escalonamento.java
@@ -1,21 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package escalonamento;
+
+import java.util.ArrayList;
 
 /**
  *
- * @author pdurand
+ * @author Patricia Durand e Tamires Domingues
  */
 public class Escalonamento {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Arquivo a = new Arquivo();
+        a.leArquivo("arquivo.txt");
+
+        ArrayList<String> dados = a.pegaDadosDoArquivo();
+
+        for (int i = 0; i < dados.size(); i++) {
+            System.out.println(dados.get(i));
+        }
     }
-    
+
 }
