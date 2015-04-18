4f3596ccb2a46f020024751d59e0993f53086df6
PatriciaDurand <durand_patricia_@hotmail.com>
2015-04-18 10:40:53 -0300

    adicionado a logica para verificar se um processo chegou

--- src/escalonamento/SJF.java
+++ src/escalonamento/SJF.java
@@ -9,20 +9,22 @@ import java.util.ArrayList;
 public class SJF {
 
     private ArrayList<Processo> listaDeProcessos;
+    private ArrayList<Processo> listaParaOrdenar;
     private ArrayList<String> graficoSaida;
     int tempo = 0;
+    int tempoVerificacaoChegada = 0;
 
     public SJF(ArrayList<Processo> listaDeProcessos) {
         this.listaDeProcessos = listaDeProcessos;
+        listaParaOrdenar = new ArrayList<Processo>();
         graficoSaida = new ArrayList<String>();
         executaEscalonamento();
     }
 
     private void executaEscalonamento() {
-        if (listaDeProcessos != null && !listaDeProcessos.isEmpty()) {
+        if (listaDeProcessos != null) {
             if (processoChegou()) {
                 atualizaFila();
-
             }
             if (temProcessoNaFila()) {
                 executaProcesso();
@@ -34,11 +36,17 @@ public class SJF {
     }
 
     private boolean processoChegou() {
-        return true;
+        for (int i = 0; i < listaDeProcessos.size(); i++) {
+            if (listaDeProcessos.get(i).getTempoDeChegada() <= tempo
+                    && listaDeProcessos.get(i).getTempoDeChegada() > tempoVerificacaoChegada) {
+                listaParaOrdenar.add(listaDeProcessos.get(i));
+                return true;
+            }
+        }
+        return false;
     }
 
     private void atualizaFila() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     private boolean temProcessoNaFila() {
@@ -52,5 +60,4 @@ public class SJF {
     private void atualizaContador() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
 }
